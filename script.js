366a7469e58bd0e37013ddcfbf43ed798a0c9807
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-02-12 06:52:17 -0300

    implementada funcionalidade de mandar mensagem reservada ou publica escolhendo o usuario

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const MAGIC = true
+const MESSAGESUPDATETIME = 3000
+const STATUSUPDATETIME = 50000
+const USERSONLINEUPDATETIME = 10000
+
 // Entrar na sala
 let userName
 let nameObject = {
@@ -38,10 +42,14 @@ function serverTestLoginError() {
 // status usuario
 function userStatus() {
     nameObject.name = userName
-    axios.post("https://mock-api.driven.com.br/api/v4/uol/status", nameObject).then(teste())
+    axios.post("https://mock-api.driven.com.br/api/v4/uol/status", nameObject).then(teste()).catch(reload)
 }
-function teste(){
+function teste() {
     console.log("status enviado");
+}
+function reload() {
+    console.log("reload")
+    location.reload();
 }
 if (MAGIC) {
     console.log("status");
@@ -52,27 +60,36 @@ if (MAGIC) {
 // Faz uma requisição ao servidor para procurar pelas mensagens
 function searchMessages() {
     let promisse = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages")
-    promisse.then(renderTheMessages)
+    promisse.then(renderMessages)
 }
 if (MAGIC) {
     setInterval(() => {
+        console.log("Procurando mensagens")
         searchMessages()
-    }, 500);
+    }, MESSAGESUPDATETIME);
 }
-let count = 0
-let messageListLog = []
-let messagesList = []
+let index = 0
+let firstRun = true
+let messageListLog = ""
 let filteredMessages = []
-function renderTheMessages(success) {
-    messagesList = success.data
+function renderMessages(success) {
+    let messagesList = success.data
     const messages = document.querySelector(".messages")
-    //filteredMessages = messagesList.filter(x => !messageListLog.includes(x))
-    //for (let i = 0; i <= messagesList.length; ++i) {
-        //console.log("entrei");
-        if(count == 0)
-            messageListLog = messagesList.slice()
-        if (messagesList[99].text != messageListLog[99].text){
-        let i = 99
+    // Verifica se é a primeira vez que a função é executada
+    /*if (!firstRun) {
+        // Compara o ultimo elemento da lista de mensagens com a ultima mensagem armazenada
+        for (let i = 99; i; i--) {
+            if (messageListLog == (messagesList[i].from + messagesList[i].text + messagesList[i].time)) {
+                i = index
+                console.log("vou parar");
+                break
+            }else if(i == 0){
+                i = index
+            }
+        }
+    }*/
+    messages.innerHTML = ""
+    for (let i = 0; i < messagesList.length; ++i) {
         if (messagesList[i].type == "status") {
             messages.innerHTML +=
                 `
@@ -87,7 +104,7 @@ function renderTheMessages(success) {
             <p><span class="hour">(${messagesList[i].time}) </span> <span class="user"> ${messagesList[i].from} </span> para <span class="user"> ${messagesList[i].to}: </span> ${messagesList[i].text} </p>
             </div>
             `
-        } else if (messagesList[i].type == "private_message" && messagesList[i].to == userName) {
+        } else if (messagesList[i].type == "private_message" && (messagesList[i].to == userName || messagesList[i].to == "Todos" || messagesList[i].from == userName)) {
             messages.innerHTML +=
                 `
             <div class="message private">
@@ -95,23 +112,26 @@ function renderTheMessages(success) {
             </div>
             `
         }
-        messages.scrollIntoView({ block: "end", behavior: "smooth" });
+        //, behavior: "smooth" 
+        messages.scrollIntoView({ block: "end" });
     }
-    count +=1
-    messageListLog = messagesList.slice()
+    //firstRun = false
+    //messageListLog = messagesList[99].from + messagesList[99].text + messagesList[99].time
 }
 
-
+let userSelected = "Todos"
+let messageVisibility = "message"
 function sendMessage() {
     const valueInput = document.querySelector("footer input")
+    console.log(userName, userSelected, valueInput.value, messageVisibility + "  aaaaaaaaaaaaaa");
     // Enviar mensagem teclando "Enter"
     valueInput.addEventListener('keydown', function (event) {
         if (event.keyCode == 13) {
             const messageObjetc = {
                 from: userName,
-                to: "Todos",
+                to: userSelected,
                 text: valueInput.value,
-                type: "message"
+                type: messageVisibility
             }
             valueInput.value = ""
             axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", messageObjetc).then(testarEnvio)
@@ -121,9 +141,9 @@ function sendMessage() {
     if (valueInput.value != "") {
         const messageObjetc = {
             from: userName,
-            to: "Todos",
+            to: userSelected,
             text: valueInput.value,
-            type: "message"
+            type: messageVisibility
         }
         valueInput.value = ""
         axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", messageObjetc).then(testarEnvio)
@@ -139,18 +159,33 @@ function searchUsers() {
     axios.get("https://mock-api.driven.com.br/api/v4/uol/participants").then(renderUsers)
 }
 
+setInterval(() => {
+    searchUsers()
+}, USERSONLINEUPDATETIME);
+
+let usersListLog = []
 function renderUsers(users) {
     users = users.data
-    const usersClass = document.querySelector(".activity")
-    for (let i = 0; i < users.length; ++i) {
+    const usersClass = document.querySelector(".activity .users")
+    let filteredUsers = users.filter(filterUsers);
+    console.log(filteredUsers);
+    for (let i = 0; i < filteredUsers.length; ++i) {
         usersClass.innerHTML +=
             `
         <div class="option">
-                    <ion-icon name="person-circle" onclick=""></ion-icon>
-                    <p onclick="">${users[i].name}</p>
+                    <ion-icon name="person-circle" onclick="selectUser(this)"></ion-icon>
+                    <p onclick="selectUser(this)">${filteredUsers[i].name}</p>
                 </div>
         `
     }
+    usersListLog = users.slice()
+}
+function filterUsers(users) {
+    for (let i = 0; i < usersListLog.length; i++) {
+        if (users.name == usersListLog[i].name)
+            return false
+    }
+    return true
 }
 
 function activity() {
@@ -163,10 +198,51 @@ function exitActivity(shadowClass) {
     shadowClass.parentNode.classList.add("hidden")
 }
 
-function selectUser(){
-    
+function selectUser(element) {
+    const optionClass = element.parentNode
+    const usersClass = optionClass.parentNode
+    const userName = optionClass.querySelector("p").innerHTML
+    // Caso o elemento a ser clicado já não esteja selecionado seleciona o mesmo
+    if (usersClass.querySelector(".selected") != optionClass) {
+        usersClass.querySelector(".selected").remove()
+        optionClass.innerHTML +=
+            `
+        <div class="selected">
+            <svg width="13" height="11" viewBox="0 0 13 11" fill="none" xmlns="http://www.w3.org/2000/svg"> 
+            <path d="M11 2L4.7 9L2 6.375" stroke="#28BB25" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+        </div>
+        `
+    }
+    document.querySelector("footer p span").innerHTML = userName
+    userSelected = userName
+}
+function selectVisibility(element) {
+    const optionClass = element.parentNode
+    const visibilityClass = optionClass.parentNode
+    const visibility = optionClass.querySelector("p").innerHTML
+    if (visibilityClass.querySelector(".selected") != optionClass) {
+        visibilityClass.querySelector(".selected").remove()
+        optionClass.innerHTML +=
+            `
+        <div class="selected">
+            <svg width="13" height="11" viewBox="0 0 13 11" fill="none" xmlns="http://www.w3.org/2000/svg"> 
+            <path d="M11 2L4.7 9L2 6.375" stroke="#28BB25" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+        </div>
+        `
+
+    }
+    console.log(visibility);
+    if (visibility == "Reservadamente") {
+        document.querySelector("footer p b").innerHTML = "(reservadamente)"
+        messageVisibility = "private_message"
+    }
+    else {
+        document.querySelector("footer p b").innerHTML = "(público)"
+        messageVisibility = "message"
+    }
 }
 
-
 login()
-searchUsers()
+
